a58e9fd690a590b397fafb06c0e6939c72f33e6f
ryota1993 <wassupmprrepwinfield@gmail.com>
2020-02-01 14:31:50 +0900

    Added login

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+#app.py
+######  Flask related Libraries   ############
 from flask import Flask
+from apis import api
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
+######  Flask&API settings  ############
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "secret"
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['JWT_SECRET_KEY'] = 'super-secret-fishing'
 
-@app.route("/",methods=["POST","GET"])
-def index():
-    return "Welcome to Home"
+api.init_app(app)
+jwt = JWTManager(app)
+cors = CORS(app)
+
+######  Run the app   ###########
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
